7b45a65276153092a175cee24edab3d72ec2b875
Linh Pham <linh.pham@rea-group.com>
2015-07-13 12:10:09 +1000

    Level 3 done

--- rubywarrior/linh-beginner/player.rb
+++ rubywarrior/linh-beginner/player.rb
@@ -1,9 +1,17 @@
 class Player
   def play_turn(warrior)
     if warrior.feel.empty?
-      warrior.walk!
+      if not_well?(warrior)
+        warrior.rest!
+      else
+        warrior.walk!
+      end
     else
       warrior.attack!
     end
+  end
+
+  def not_well?(warrior)
+    warrior.health <= 10
   end
 end
