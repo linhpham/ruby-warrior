908fea59d1f0f9d94eb3a878673a38e8709575a0
Linh Pham <linh.pham@rea-group.com>
2015-07-13 12:13:57 +1000

    Level 6 done

--- rubywarrior/linh-beginner/player.rb
+++ rubywarrior/linh-beginner/player.rb
@@ -1,29 +1,63 @@
 class Player
   MAX_HEALTH = 20
-  HEALTH_THRESHOLD = 15
+  HEALTH_THRESHOLD = 8
 
   def initialize
     @health = MAX_HEALTH
+    @direction = :forward
+    @captured = false
   end
 
   def play_turn(warrior)
-    if warrior.feel.empty?
+    @direction = think_direction(warrior, @direction, @captured)
+
+    puts "last hp = #{@health}, current hp = #{warrior.health}"
+    if warrior.feel(@direction).empty?
       if not_well?(warrior) && is_safe?(warrior)
         warrior.rest!
       else
-        warrior.walk!
+        warrior.walk!(@direction)
       end
-    elsif warrior.feel.captive?
-      warrior.rescue!
+    elsif warrior.feel(@direction).captive?
+      warrior.rescue!(@direction)
+      @captured = true
     else
-      warrior.attack!
+      warrior.attack!(@direction)
     end
 
     @health = warrior.health
   end
 
-  def not_well?(warrior)
+  def think_direction(warrior, direction, captured)
+    if warrior.feel(direction).wall?
+      return (direction == :forward ? :backward : :forward)
+    end
+
+    unless captured
+      return :backward
+    end
+
+    if warrior.feel.empty? && is_low_hp?(warrior) && !is_safe?(warrior)
+      return :backward
+    end
+
+    if is_well?(warrior) && is_safe?(warrior)
+      return :forward
+    end
+
+    return :forward
+  end
+
+  def is_low_hp?(warrior)
     warrior.health <= HEALTH_THRESHOLD
+  end
+
+  def not_well?(warrior)
+    !is_well?(warrior)
+  end
+
+  def is_well?(warrior)
+    warrior.health >= MAX_HEALTH
   end
 
   def is_safe?(warrior)
